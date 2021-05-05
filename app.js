b3be9a45a9e41b40e4015e5468ec6a343788e4ff
maildaltontavares <dalton_tavares@bol.com.br>
2021-05-05 17:05:38 -0300

    05052021g

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require("./config/server");   
  
-PORT = process.env.PORT || 3000;
-app.listen(PORT,function(){
+var porta = process.env.PORT || 3000;
+app.listen(porta,function(){
     console.log('Servidor ativo na porta 3000.');
 }); 
