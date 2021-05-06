ee29d1b54ff708f448cb2d82dce339bbf9faaa47
maildaltontavares <dalton_tavares@bol.com.br>
2021-05-06 16:48:18 -0300

    06052021h

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,10 +1,96 @@
 var crypto = require('crypto');
 const consts = require('../../consts.js');
 const jwt = require('jsonwebtoken');  
+ 
+function criptografa(usuario) { 
+
+    try {
+         
+        let senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");  
+        return  senha_criptografada ;   
+    }
+    catch(e) {
+        res.status(500).json({message: 'Erro na execução', error: e});
+    }  
+
+}
+
+
+function gerar_token(req, res) {    
+    try {
+        var token = jwt.sign({ foo: 'bar' }, consts.keyJWT,{expiresIn: consts.expiresJWT});   
+        //console.log(token);
+        //let token = jwt.sign({_id: user._id}, consts.keyJWT,{expiresIn: consts.expiresJWT}); 
+        return res.status(200).json({message: token}); 
+    }
+    catch(e) {
+        res.status(500).json({message: 'Erro na execução', error: e});
+    }   
+
+}  
+
+function gerar_pwd_cripto(req, res) {    
+    try {
+
+        //console.log(req.body);
+        var user = req.body; 
+        let pwd_cript  =  criptografa(user);  
+        
+        return res.status(200).json({...user, senha: pwd_cript});
+
+    }
+    catch(e) {
+        res.status(500).json({message: 'Erro na execução gera_usr 999', error: e});
+    }   
+
+} 
+
+
+module.exports= {  gerar_token, gerar_pwd_cripto }
+
 
  
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //module.exports.criptografa = function(application, req, res){
+
+
+/* 
 
 module.exports = {
 
@@ -56,7 +142,7 @@ module.exports = {
 } 
 
     
-/*    
+   
     register: async function(req, res) {
         try {
             let u = await UserModel.findOne({email: req.body.email});
--- app/rotas/auth.js
+++ app/rotas/auth.js
@@ -1,68 +1,11 @@
-//Excluir
-var crypto = require('crypto');
-const consts = require('../../consts.js');
-const jwt = require('jsonwebtoken');    
-//const app = require('../../config/server.js');
-//const ctrl = require("../../app/controllers/authController.js") ;
-const app = require("../../app/controllers/authController.js") ;
-const testeApi = require("../../app/controllers/funcoes.js") ;
-//Excluir
-
-module.exports=function(application){ 
  
-    
-    application.get('/teste1',testeApi.teste);
-
-    application.get('/gt',testeApi.gerar_token);
-
-    application.post('/gc',testeApi.gerar_pwd_cripto);
-
-    application.get('/auth_jwt',function(req,res)
-    {  
-        //console.log(req.headers);  
-        application.app.controllers.authController.gera_token(application,req,res);
-    });    
-
-    application.post('/auth_cript',function(req,res)
-    { 
-        application.app.controllers.authController.gera_pwd_cripto(application,req,res);
-    });    
-
-    application.get('/testeJWT',function(req,res)
-    {
-         
-        try {
+const api = require("../../app/controllers/authController.js") ;
  
-            var token = jwt.sign({ foo: 'bar' }, consts.keyJWT,{expiresIn: consts.expiresJWT});   
-            //console.log(token);
-            //let token = jwt.sign({_id: user._id}, consts.keyJWT,{expiresIn: consts.expiresJWT}); 
-            return res.status(200).json({message: token}); 
-        }
-        catch(e) {
-            
-            res.status(500).json({message: 'Erro na execução', error: e});
-        }    
- 
-    }); 
 
-    application.post('/testeCripto',function(req,res)
-    {
+module.exports=function(application){  
 
-        try {
-            var user = req.body; 
-            let pwd_cript  = '12332112331'; //this.criptografa(user);   
-            //console.log(pwd_cript.message);
-            
-             return res.status(200).json({...user, senha: pwd_cript}); 
-        }
-        catch(e) {
-            res.status(500).json({message: 'Erro na execução gera_usr', error: e});
-        }    
- 
-    });     
+    application.get('/auth_jwt',api.gerar_token);
 
-
-    
+    application.post('/auth_cripto',api.gerar_pwd_cripto); 
 
 }
- 
