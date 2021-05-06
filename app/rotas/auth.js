6ed687d1ec7c80ef658767ff2ac987ed6e437f9c
maildaltontavares <dalton_tavares@bol.com.br>
2021-05-06 11:46:26 -0300

    06052021b

--- app/rotas/auth.js
+++ app/rotas/auth.js
@@ -1,4 +1,9 @@
-  
+//Excluir
+var crypto = require('crypto');
+const consts = require('../../consts.js');
+const jwt = require('jsonwebtoken');    
+//Excluir
+
 module.exports=function(application){
  
    // application.get('/auth_cript',function(req,res)
@@ -16,10 +21,7 @@ module.exports=function(application){
     });    
 
     application.post('/auth_cript',function(req,res)
-    {
-        //console.log(req.headers); 
-       // console.log("req.headers"); 
-         
+    { 
         application.app.controllers.authController.gera_pwd_cripto(application,req,res);
     });    
 
@@ -28,17 +30,19 @@ module.exports=function(application){
 
         //console.log(req.headers); 
         //console.log("req.headers");         
-        res.send('<html><body>Arquivos BD123</body></html>');
+        //res.send('<html><body>Arquivos BD123</body></html>');
         try {
+            //res.send('<html><body>Arquivos BD456</body></html>');
             var token = jwt.sign({ foo: 'bar' }, consts.keyJWT,{expiresIn: consts.expiresJWT});   
             //console.log(token);
             //let token = jwt.sign({_id: user._id}, consts.keyJWT,{expiresIn: consts.expiresJWT}); 
             return res.status(200).json({message: token}); 
         }
         catch(e) {
+            //res.send('<html><body>Arquivos BD456</body></html>');
             res.status(500).json({message: 'Erro na execução', error: e});
         }   
-        res.send('<html><body>Arquivos BD456</body></html>');
+        
 
 
 
