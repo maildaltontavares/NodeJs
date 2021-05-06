dad00ea5e9566d93b38ca2c5fd8165bca7ce4f7b
maildaltontavares <dalton_tavares@bol.com.br>
2021-05-06 16:31:33 -0300

    06052021g

--- app/controllers/funcoes.js
+++ app/controllers/funcoes.js
@@ -39,16 +39,16 @@ function gerar_token(req, res) {
 
 function gerar_pwd_cripto(req, res) {    
     try {
+
+        //console.log(req.body);
         var user = req.body; 
-        let pwd_cript  =  this.criptografa(user);  
+        let pwd_cript  =  criptografa(user);  
         
-        console.log(pwd_cript);
-        
-         return res.status(200).json({...user, senha: pwd_cript});
+        return res.status(200).json({...user, senha: pwd_cript});
 
     }
     catch(e) {
-        res.status(500).json({message: 'Erro na execução gera_usr', error: e});
+        res.status(500).json({message: 'Erro na execução gera_usr 999', error: e});
     }   
 
 } 
